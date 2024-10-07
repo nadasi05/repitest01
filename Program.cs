8e837e07822f5ef14e3e223bb6a50dd3aec551fd
nadasi05 <nadabalint2002@gmail.com>
2024-10-07 16:25:28 +0200

    csharpproba

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace gyakorlas1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("szia bálint");
+            Console.WriteLine("hogy vagy?");
+            Console.Beep();
+
+        }
+
+
+
+    }
+
+
+
+
+}
